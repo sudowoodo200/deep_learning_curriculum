5661a5a3faac0bd958c7bd755f9c47d1160438d3
Sen Lin <lin.sen200@gmail.com>
2023-02-09 16:52:23 -0800

    Model scaffold

--- Practice/Transformers/main.py
+++ Practice/Transformers/main.py
@@ -14,16 +14,50 @@ from train_utils import *
 ## Model Definition
 class DecoderModel(nn.Module):
 
-    def __init__(self):
+    def __init__(self, vocab_size, channel_size, block_length):
 
         super().__init__()
-        
+        self.channel_embedding = nn.Embedding(vocab_size, channel_size)
+        self.positional_encoding = nn.Embedding(block_length, channel_size)
+        self.main_block = nn.Linear( block_length * channel_size, block_length * channel_size) # placeholder
+        self.ln_final = nn.LayerNorm(channel_size)
+        self.out = nn.Linear(channel_size, vocab_size)
 
     def forward(self, x, y = None):
-        raise NotImplementedError
+
+        batch_size, block_length = x.shape
+        token_embedding = self.channel_embedding(x)  # (batch_size, block_length, channel_size)
+        positional_embedding = self.positional_encoding(torch.arange(block_length).repeat(batch_size))  # (batch_size, block_length, channel_size)
+        embedding = token_embedding + positional_embedding  # (batch_size, block_length, channel_size)
+        
+        # placeholder
+        z = embedding.view(batch_size, -1) # (batch_size, block_length * channel_size)
+        z = self.main_block(z) # (batch_size, block_length * channel_size)
+        embedding = z.view(batch_size, block_length, -1) # (batch_size, block_length, channel_size)
+
+        # output
+        embedding = self.ln_final(embedding) # (batch_size, block_length, channel_size)
+        output = self.out(embedding) # (batch_size,, block_length, vocab_size)
+
+        # compute loss if applicable
+        if y is not None:
+            loss = self.compute_loss(output, y)
+        else:
+            loss = None
+
+        return output, loss
 
     def generate(self):
         raise NotImplementedError
+
+    def compute_loss(self, output, y):
+
+        batch_size, block_length, vocab_size = output.shape
+        logits = output.view(-1, vocab_size) # (batch_size * block_length, vocab_size)
+        target = y.view(-1) # (batch_size * block_length)
+        loss = F.cross_entropy(logits, target, ignore_index=0)
+
+        return loss
 
 ## Training
 def train(model, optimizer, criterion, data, device):
@@ -78,43 +112,35 @@ if __name__ == '__main__':
     argparse.add_argument('--data', type=str, default= None)
     argparse.add_argument('--device', type=str, default='cuda')
     argparse.add_argument('--logdir', type=str, default='logs')
-    argparse.add_argument('--epochs', type=int, default=100)
-    argparse.add_argument('--batch_size', type=int, default=32)
-    argparse.add_argument('--learning_rate', type=float, default=0.001)
-    argparse.add_argument('--embedding_size', type=int, default=256)
-    argparse.add_argument('--block_length', type=int, default=10)
 
     args = argparse.parse_args()
     
     if (args.task == "char_reversal"):
 
-        n_epochs = args.epochs
-        batch_size = args.batch_size
-        learning_rate = args.learning_rate
-        block_length = args.block_length
-        embedding_size = args.embedding_size
+        n_epochs = 10
+        iter_per_epoch = 100        
+        batch_size = 32
+        learning_rate = 0.001
+        block_length = 10
+        channel_size = 256
 
         gen = CharacterReversalDatagen(length=block_length)
 
         if args.data is None:
-
             df = gen.generate_data(1e5)
-            df = gen.encode_data(df)
-            train_df, test_df = split_data(df, test_size= 0.2) # (N x L)
         
         else:
-
             data_file = input("Enter the data file to upload: ")
             df = pd.read_csv(data_file)
-            df = gen.encode_data(df)
-            train_df, test_df = split_data(df, test_size= 0.2)
+
+        df = gen.encode_data(df)
+        train_df, test_df = split_data(df, test_size= 0.2) # (N x L)
+        train_x, train_y = to_tensor(train_df)
+        test_x, test_y = to_tensor(test_df)
 
     elif (args.task == "shakespeare"):
         raise NotImplementedError("Shakespeare task not implemented")
     
     else:
         raise ValueError("Invalid task")
-
-
-
 
